13bb84839320c4965d07c82ead622897a120631c
Tal Liron <tal.liron@gmail.com>
2009-04-19 21:02:49 +0000

    git-svn-id: https://scripturian.googlecode.com/svn/trunk@68 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/CompositeScriptContext.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/CompositeScriptContext.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2009 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of the LGPL version 3.0:
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://www.threecrickets.com/
+ */
+
 package com.threecrickets.scripturian;
 
 import java.io.Writer;
