27f46ddca0a2e5b4ea4166256015d06b64affa4b
Tal Liron <tal.liron@gmail.com>
2010-01-06 02:15:36 +0000

    Happy new year!
    git-svn-id: https://scripturian.googlecode.com/svn/trunk@102 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/helper/VelocityScriptletParsingHelper.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/helper/VelocityScriptletParsingHelper.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2009 Three Crickets LLC.
+ * Copyright 2009-2010 Three Crickets LLC.
  * <p>
  * The contents of this file are subject to the terms of the LGPL version 3.0:
  * http://www.opensource.org/licenses/lgpl-3.0.html
  * <p>
  * Alternatively, you can obtain a royalty free commercial license with less
  * limitations, transferable or non-transferable, directly from Three Crickets
- * at http://www.threecrickets.com/
+ * at http://threecrickets.com/
  */
 
 package com.threecrickets.scripturian.helper;
--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/internal/ServiceLoader.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/internal/ServiceLoader.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2009-2010 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of the LGPL version 3.0:
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://threecrickets.com/
+ */
+
 package com.threecrickets.scripturian.internal;
 
 import java.io.BufferedReader;
