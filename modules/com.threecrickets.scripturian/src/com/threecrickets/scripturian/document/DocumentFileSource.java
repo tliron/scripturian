0b7d5a1d36e035071c2cc02d414123171e203d7e
Tal Liron <tal.liron@gmail.com>
2010-12-15 19:49:43 +0000

    Code cleanup
    git-svn-id: https://scripturian.googlecode.com/svn/trunk@253 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/document/DocumentFileSource.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/document/DocumentFileSource.java
@@ -13,7 +13,6 @@ package com.threecrickets.scripturian.document;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
