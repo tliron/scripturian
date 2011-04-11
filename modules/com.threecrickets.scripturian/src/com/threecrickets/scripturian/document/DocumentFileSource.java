a7a8624e6032d661a807faee7dcd9d1678eb9a3d
Tal Liron <tal.liron@gmail.com>
2011-04-11 06:36:53 +0000

    Fixed file-not-found logic
    git-svn-id: https://scripturian.googlecode.com/svn/trunk@276 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/document/DocumentFileSource.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/document/DocumentFileSource.java
@@ -304,7 +304,8 @@ public class DocumentFileSource<D> implements DocumentSource<D>
 					filedDocumentDescriptorsByAlias.put( documentName, filedDocumentDescriptor );
 			}
 		}
-		else if( ( filedDocumentDescriptor.file == null ) || !filedDocumentDescriptor.file.exists() )
+
+		if( ( filedDocumentDescriptor.file == null ) || !filedDocumentDescriptor.file.exists() )
 			throw new DocumentNotFoundException( "Document descriptor's file does not exist: " + documentName );
 
 		return filedDocumentDescriptor;
