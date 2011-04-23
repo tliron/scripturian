c61c731acaafe94cccbd3071ee4d010cba11df44
Tal Liron <tal.liron@gmail.com>
2011-04-23 04:38:27 +0000

    Cleaner DocumentNotFoundException for document.execute
    git-svn-id: https://scripturian.googlecode.com/svn/trunk@284 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/service/DocumentService.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/service/DocumentService.java
@@ -272,7 +272,7 @@ public class DocumentService
 				}
 
 				if( ( iterator == null ) || !iterator.hasNext() )
-					throw x;
+					throw new DocumentNotFoundException( documentName );
 
 				parsingContext.setDocumentSource( iterator.next() );
 			}
