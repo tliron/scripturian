1880f601e0de27b485a57334361e5f99e42a875f
Tal Liron <tal.liron@gmail.com>
2013-12-28 16:10:50 +0800

    Update copyright notices for 2014

--- components/scripturian/source/com/threecrickets/scripturian/adapter/WikiTextAdapterBase.java
+++ components/scripturian/source/com/threecrickets/scripturian/adapter/WikiTextAdapterBase.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2009-2014 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of the LGPL version 3.0:
+ * http://www.gnu.org/copyleft/lesser.html
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://threecrickets.com/
+ */
+
 package com.threecrickets.scripturian.adapter;
 
 import java.util.Collection;
